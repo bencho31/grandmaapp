9a5e39bd0cfe418979747301437eac6f9e1410da
yoann benchetrit <benchetrityoann@gmail.com>
2018-11-19 20:37:08 +0200

    its possible to list allof grandam thanks you ben for all

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_114520) do
+ActiveRecord::Schema.define(version: 2018_11_19_182046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_11_19_114520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.string "photo"
   end
 
   create_table "reviews", force: :cascade do |t|
