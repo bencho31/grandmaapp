25a078fe81c8cddacdc92fa933e6b8cb6d0440b7
yoann benchetrit <benchetrityoann@gmail.com>
2018-11-19 14:31:01 +0200

    formindex

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Grandma.destroy_all
+
+Grandma.create!([{
+  name: "Jaqueline",
+  email: "jaqueline@gmail.com",
+  description: "Sexy and i love to cook",
+  category: "sepharade"
+},
+{
+  name: "Michel",
+  email: "michel@gmail.com",
+  description: "Sexy and i love to cook",
+  category: "sepharade"
+},
+{
+  name: "Jeanne",
+  email: "jeanne@gmail.com",
+  description: "Sexy and i love to cook",
+  category: "ashkenazi"
+}])
+
+p "Created #{Grandma.count} movies"
