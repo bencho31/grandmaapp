5f790fde6b6080af4a85d72a27b96e348bf339c3
Thomas Freund <tfreundc@gmail.com>
2018-11-22 18:28:45 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@
 
 
 require 'faker'
-
+User.destroy_all
+Review.destroy_all
+Booking.destroy_all
 Grandma.destroy_all
 
 
@@ -17,7 +19,7 @@ Grandma.create(
   email:"llama.llama@gmail.com",
   description:"I am just a llama.",
   category: "sepharade",
-  photo: "http://res.cloudinary.com/dseugxpvh/image/upload/v1542879313/vp9py2iutdkuu5uwrkyw.jpg",
+  remote_photo_url: "http://res.cloudinary.com/dseugxpvh/image/upload/v1542879313/vp9py2iutdkuu5uwrkyw.jpg",
   price: 100
 )
 Grandma.create(
@@ -25,7 +27,7 @@ Grandma.create(
   email:"whatisanemail@gmail.com",
   description:"I love to cook all kinds of things. If you want something, ask me!",
   category: "sepharade",
-  photo: "http://res.cloudinary.com/dseugxpvh/image/upload/v1542891564/ces-remedes-de-grand-mere-qui-nous-aident-a-aller-mieux.jpg",
+  remote_photo_url: "http://res.cloudinary.com/dseugxpvh/image/upload/v1542891564/ces-remedes-de-grand-mere-qui-nous-aident-a-aller-mieux.jpg",
   price: 40
 
 )
@@ -34,7 +36,7 @@ Grandma.create(
   email:"whatisanemail@gmail.com",
   description:"I like to work with people who love to eat. I am very nice and friendly and kids love to eat my meals.",
   category: "sepharade",
-  photo: 'http://res.cloudinary.com/dseugxpvh/image/upload/v1542807450/grandma.jpg',
+  remote_photo_url: 'http://res.cloudinary.com/dseugxpvh/image/upload/v1542807450/grandma.jpg',
   price: 23
 
 )
@@ -44,7 +46,7 @@ Grandma.create(
   email:"whatisanemail@gmail.com",
   description:"I know how to make the best !",
   category: "ashkenazi",
-  photo: "http://res.cloudinary.com/dseugxpvh/image/upload/v1542812258/en2vvn0khqxj491lexuk.jpg",
+  remote_photo_url: "http://res.cloudinary.com/dseugxpvh/image/upload/v1542812258/en2vvn0khqxj491lexuk.jpg",
   price: 23
 )
 
@@ -55,7 +57,7 @@ Grandma.create(
     email:Faker::Internet.email,
     description:Faker::Hobbit.quote,
     category: "sepharadi",
-    photo: "https://res.cloudinary.com/dseugxpvh/image/upload/v1542891565/b9a9d131-9c1b-4990-b3d2-0e0355989468-fotolia_188948342_subscription_monthly_m.jpg",
+    remote_photo_url: "https://res.cloudinary.com/dseugxpvh/image/upload/v1542891565/b9a9d131-9c1b-4990-b3d2-0e0355989468-fotolia_188948342_subscription_monthly_m.jpg",
     price: 23
   )
 end
