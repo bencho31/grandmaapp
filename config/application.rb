70aed4afbadd4156e60c4af57883fa2f50178f87
Thomas Freund <tfreundc@gmail.com>
2018-11-22 18:00:58 +0200

    same

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Grandmaapp
           generate.assets false
           generate.helper false
           generate.test_framework  :test_unit, fixture: false
+          config.assets.initialize_on_precompile = false
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
